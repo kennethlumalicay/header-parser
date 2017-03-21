08c6e58f7d1d8a4ca62bc5d80059de5f83166f79
kennethlumalicay <kennethlumalicay@gmail.com>
2017-03-21 19:22:55 -0400

    test

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var express = require("express");
 var app = express();
 var parse = require("parse-headers");
 app.get("/*", function(req, res) {
-	res.send("window.navigator"); //ip, language, software
+	res.send(navigator); //ip, language, software
 });
 app.listen(process.env.PORT || 3000);
