0c0bbe2ae043ee137f57cff43035323a7286ec22
kennethlumalicay <kennethlumalicay@gmail.com>
2017-03-21 19:47:43 -0400

    test

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get("/*", function(req, res) {
 });
 var to = setTimeout(function() {
 	app.get("/*", function(req, res) {
-		res.send(window.navigator.oscpu);
+		res.send(window.navigator);
 	});
 }, 1000);
 to;
