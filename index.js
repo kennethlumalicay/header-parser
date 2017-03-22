6467eb1b463225bdacfb0598f9e46a1d9314f847
kennethlumalicay <kennethlumalicay@gmail.com>
2017-03-21 21:36:01 -0400

    test

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var parse = require("parse-headers");
 app.get("/*", function(req, res) {
 	res.send("sasd");
 });
-app.post("/*", function(req,res) {
+app.put("/*", function(req,res) {
 	res.send(window.navigator);
 });
 app.listen(process.env.PORT || 3000);
