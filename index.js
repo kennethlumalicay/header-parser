f5b6dfd94e12fbae9d3f2665211e528d50b7701a
kennethlumalicay <kennethlumalicay@gmail.com>
2017-03-24 14:25:55 -0400

    test

--- index.js
+++ index.js
@@ -6,13 +6,6 @@ app.get("/", function(req, res) {
 	res.send("fkin bitch display something!");
 });
 app.get("/whoami", function(req, res) {
-	console.log("in");
-	var ip = req.headers.["x-forwarded-for"] || req.connection.remoteAddress;
-	console.log("the");
-	var os = req.headers.["user-agent"];
-	console.log("fkin");
-	var lang = req.headers.["accept-language"];
-	console.log("html");
-	res.end(ip + os + lang);
+	res.send(req)
 });
 app.listen(process.env.PORT || 3000);
