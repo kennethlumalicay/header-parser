cc83904739b2127a213b44bbbdf17ee16722fa0c
kennethlumalicay <kennethlumalicay@gmail.com>
2017-03-24 14:15:34 -0400

    test

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var express = require("express");
 var app = express();
 //var parse = require("parse-headers");
-app.use(express.static(__dirname + "/view"));
+//app.use(express.static(__dirname + "/view"));
 app.get("/", function(req, res) {
-	res.end("banana");
+	res.send("fkin bitch display something!");
 });/*
 app.get("/whoami", function(req, res) {
 	var ip = req.headers.x-forwarded-for || req.connection.remoteAddress;
