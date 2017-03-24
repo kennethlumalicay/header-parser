8c820d52db52d567a29304f8cde159492f53c7da
kennethlumalicay <kennethlumalicay@gmail.com>
2017-03-24 15:41:50 -0400

    working

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ app.get("/*", function(req, res) {
 	res.send({
 		ipaddress: req.ip.replace("::ffff:", ""),
 		language: req.get("accept-language").split(/,/)[0],
-		software: req.get("user-agent").split(/\(\)/g)[1]
+		software: req.get("user-agent").split(/[()]/g)[1]
 	});
 });
 app.listen(process.env.PORT || 3000);
