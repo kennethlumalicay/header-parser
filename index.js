84d40d741578393283b129b15b8c61fa8eef42f7
kennethlumalicay <kennethlumalicay@gmail.com>
2017-03-24 14:11:31 -0400

    test

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ var app = express();
 app.use(express.static(__dirname + "/view"));
 app.get("/", function(req, res) {
 	res.send("banana");
-});
+});/*
 app.get("/whoami", function(req, res) {
 	var ip = req.headers.x-forwarded-for || req.connection.remoteAddress;
 	var os = req.headers.user-agent;
 	var lang = req.headers.accept-language;
 	res.end(ip + os + lang);
-});
+});*/
 app.listen(process.env.PORT || 3000);
