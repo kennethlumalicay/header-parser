f0a9b9b2384092ce12a404ea44110b69533280d7
kennethlumalicay <kennethlumalicay@gmail.com>
2017-03-24 15:34:35 -0400

    working

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 var express = require("express");
 var app = express();
-var parse = require("parse-headers");
-//app.use(express.static(__dirname + "/view"));
-app.get("/", function(req, res) {
-	res.send("fkin bitch display something!");
-});
-app.get("/whoami", function(req, res) {
-	console.log(req);
-	res.send(req);
+app.get("/*", function(req, res) {
+	res.send({
+		ipaddress: req.ip,
+		language: req.get("accept-language").split(/,/)[0],
+		software: req.get("user-agent").split(/[()]/g)[1]
+	});
 });
 app.listen(process.env.PORT || 3000);
