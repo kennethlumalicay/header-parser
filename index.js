f74e81c0f50b3b02d6846362f4a04d46e34aa080
kennethlumalicay <kennethlumalicay@gmail.com>
2017-03-24 15:37:55 -0400

    working

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 app.get("/*", function(req, res) {
 	res.send({
-		ipaddress: req.ip,
+		ipaddress: req.ip.replace("::ffff:", ""),
 		language: req.get("accept-language").split(/,/)[0],
 		software: req.get("user-agent").split(/[()]/g)[1]
 	});
