152448cdd49b64d515338a2754a70b190d56a201
kennethlumalicay <kennethlumalicay@gmail.com>
2017-03-24 13:53:12 -0400

    test

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var express = require("express");
 var app = express();
 var parse = require("parse-headers");
+app.use(express.static(__dirname + "/view"));
 app.get("/*", function(req, res) {
 	res.send("sasd");
-});
-app.put("/*", function(req,res) {
-	res.send(window.navigator);
+	var ip = req.headers.x-forwarded-for || req.connection.remoteAddress;
+	var os = req.headers.user-agent;
+	var lang = req.headers.accept-language;
+	res.end(ip + os + lang);
 });
 app.listen(process.env.PORT || 3000);
