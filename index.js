1c2ff0970a29046579c2ba3500583574cc8820d6
kennethlumalicay <kennethlumalicay@gmail.com>
2017-03-24 13:55:40 -0400

    test

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ var express = require("express");
 var app = express();
 var parse = require("parse-headers");
 app.use(express.static(__dirname + "/view"));
-app.get("/*", function(req, res) {
+app.get("/", function(req, res) {
+	res.send("banana");
+});
+app.get("/whoami", function(req, res) {
 	res.send("sasd");
 	var ip = req.headers.x-forwarded-for || req.connection.remoteAddress;
 	var os = req.headers.user-agent;
